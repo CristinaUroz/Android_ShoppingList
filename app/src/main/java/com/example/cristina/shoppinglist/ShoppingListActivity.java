316657abc670e635cca64e8a75d045811fe255a1
Cristina <cristina5696@gmail.com>
2017-10-19 15:41:58 +0200

    Commit Array Adapter

--- app/src/main/java/com/example/cristina/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/cristina/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,39 @@ package com.example.cristina.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemlist;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText)findViewById(R.id.edit_item);
+
+        itemlist=new ArrayList<>();
+        itemlist.add("patates");
+        itemlist.add("mes patates");
+        itemlist.add("moltes mes patates");
+        itemlist.add("i copes");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,itemlist);
+
+        list.setAdapter(adapter );
     }
 }
