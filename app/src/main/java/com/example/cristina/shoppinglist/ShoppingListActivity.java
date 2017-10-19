e8cf098dfb6641331c06c5ab22b6f83ea50c6a45
Cristina <cristina5696@gmail.com>
2017-10-19 16:24:34 +0200

    Shopping List 1 de 3

--- app/src/main/java/com/example/cristina/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/cristina/shoppinglist/ShoppingListActivity.java
@@ -1,12 +1,17 @@
 package com.example.cristina.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -34,7 +39,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         itemlist.add("moltes mes patates");
         itemlist.add("i copes");
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,itemlist);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,itemlist);
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,12 +48,47 @@ public class ShoppingListActivity extends AppCompatActivity {
             }
         });
 
-        list.setAdapter(adapter );
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
+
+        list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return false;
+            }
+        });
+    }
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        String fmt = getResources().getString(R.string.confirm_message);
+        builder.setMessage(String.format(fmt, itemlist.get(pos)));
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        itemlist.remove(pos);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
     }
 
     private void addItem() {
         String item_text = edit_item.getText().toString();
-        itemlist.add(item_text);
-        adapter.notifyDataSetChanged();
+        if (!item_text.isEmpty()){
+            itemlist.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
     }
 }
